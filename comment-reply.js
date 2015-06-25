124bd6991c54613cf39b145848c2635246be0118
Peter Wilson <git-commit-by@peterwilson.cc>
2015-06-25 21:56:38 +1000

    [7|8] Remove addComment Scope
    Closes #7 Move getElementById out of addComment Scope
    Closes #8 Move moveForm out of addComment scope

--- comment-reply.js
+++ comment-reply.js
@@ -88,54 +88,68 @@ PWCC.commentReply = (function( window, undefined ){
 	}
 
 
-	var addComment = {
-		moveForm : function(commId, parentId, respondId, postId) {
-			var t = this, div, comm = t.I(commId), respond = t.I(respondId), cancel = t.I('cancel-comment-reply-link'), parent = t.I('comment_parent'), post = t.I('comment_post_ID');
+	/**
+	 * Move form
+	 *
+	 * Move the comment form from the current position to the position needed for
+	 * the reply.
+	 *
+	 * @since [unknown]
+	 */
+	function moveForm(commId, parentId, respondId, postId) {
+		var t = this, div, comm = t.I(commId), respond = t.I(respondId), cancel = t.I('cancel-comment-reply-link'), parent = t.I('comment_parent'), post = t.I('comment_post_ID');
 
-			if ( ! comm || ! respond || ! cancel || ! parent )
+		if ( ! comm || ! respond || ! cancel || ! parent )
+			return;
+
+		t.respondId = respondId;
+		postId = postId || false;
+
+		if ( ! t.I('wp-temp-form-div') ) {
+			div = document.createElement('div');
+			div.id = 'wp-temp-form-div';
+			div.style.display = 'none';
+			respond.parentNode.insertBefore(div, respond);
+		}
+
+		comm.parentNode.insertBefore(respond, comm.nextSibling);
+		if ( post && postId )
+			post.value = postId;
+		parent.value = parentId;
+		cancel.style.display = '';
+
+		cancel.onclick = function() {
+			var t = addComment, temp = t.I('wp-temp-form-div'), respond = t.I(t.respondId);
+
+			if ( ! temp || ! respond )
 				return;
 
-			t.respondId = respondId;
-			postId = postId || false;
-
-			if ( ! t.I('wp-temp-form-div') ) {
-				div = document.createElement('div');
-				div.id = 'wp-temp-form-div';
-				div.style.display = 'none';
-				respond.parentNode.insertBefore(div, respond);
-			}
-
-			comm.parentNode.insertBefore(respond, comm.nextSibling);
-			if ( post && postId )
-				post.value = postId;
-			parent.value = parentId;
-			cancel.style.display = '';
-
-			cancel.onclick = function() {
-				var t = addComment, temp = t.I('wp-temp-form-div'), respond = t.I(t.respondId);
-
-				if ( ! temp || ! respond )
-					return;
-
-				t.I('comment_parent').value = '0';
-				temp.parentNode.insertBefore(respond, temp);
-				temp.parentNode.removeChild(temp);
-				this.style.display = 'none';
-				this.onclick = null;
-				return false;
-			};
-
-			try { t.I('comment').focus(); }
-			catch(e) {}
-
+			t.I('comment_parent').value = '0';
+			temp.parentNode.insertBefore(respond, temp);
+			temp.parentNode.removeChild(temp);
+			this.style.display = 'none';
+			this.onclick = null;
 			return false;
-		},
+		};
 
-		I : function(e) {
-			return document.getElementById(e);
-		}
-	};
+		try { t.I('comment').focus(); }
+		catch(e) {}
 
+		return false;
+	}
+
+
+	/**
+	 * I
+	 *
+	 * Get a DOM element by its ID
+	 *
+	 * @since [unknown]
+	 * @param {String} e the name of the element
+	 */
+	function I(e) {
+		return document.getElementById(e);
+	}
 
 
 	return {
